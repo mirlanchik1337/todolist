f07fbeae27cb02a2599b796dd6d03da7862ca701
mirlanchik1337 <sadambekovmirlan9@gmail.com>
2023-12-19 20:39:29 +0600

    asd

---
+++ todo_app/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title', '')
+        description = request.POST.get('description', '')
+        due_date = request.POST.get('due_date', '')
+        if title:
+            Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'add_task.html')
+
+def delete_task(request, task_id):
+    Task.objects.get(id=task_id).delete()
+    return HttpResponseRedirect(reverse('task_list'))
